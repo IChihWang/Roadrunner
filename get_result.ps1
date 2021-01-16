d425365e6627f826f831d3be374bc1c2a208bf9c
Michael I. -C Wang <icw238@nyu.edu>
2021-01-16 11:17:04 +0800

    Got result from desktop

--- get_result.ps1
+++ get_result.ps1
@@ -2,17 +2,12 @@
 rm result\result.csv
 
 
-For ($i=0.025; $i -le 0.75; $i+=0.05) {
+For ($i=0.0125; $i -le 0.75; $i+=0.025) {
 	For ($seed=0; $seed -le 4; $seed++) {
 		python .\main.py $i $seed 0 T
 		python .\main.py $i $seed 1 T
 		python .\main.py $i $seed 2 T
-		python .\main.py $i $seed 0 F
-	}
-}
-
-For ($i=0.025; $i -le 0.75; $i+=0.25) {
-	For ($seed=0; $seed -le 4; $seed++) {
 		python .\main.py $i $seed 3 T
+		python .\main.py $i $seed 0 F
 	}
 }
