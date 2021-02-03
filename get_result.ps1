338a1456d03e076ff03b9c20f0f9736e372140ae
Michael I. -C Wang <icw238@nyu.edu>
2021-02-03 23:47:28 +0800

    Final code

--- get_result.ps1
+++ get_result.ps1
@@ -1,7 +1,7 @@
 
-rm result/utility_time_*
+rm result/*
 
-For ($j=1; $j -le 5; $j+=1) {
+For ($j=1000; $j -le 2000; $j+=1) {
 	For ($i=0.6; $i -le 0.9; $i+=0.1) {
 		python main.py $i $j 0 T
 		python main.py $i $j 0 F
