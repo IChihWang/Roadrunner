aeb0345e9ae8b07f3a8d6e7bdb7c31b2e43a1d74
Michael I. -C. Wang <icw238@nyu.edu>
2022-05-31 21:07:03 +0800

    Disable the gui

--- get_result.ps1
+++ get_result.ps1
@@ -1,10 +1,8 @@
 rm result/*
 
 
-For ($loss=0; $loss -le 0.95; $loss+=0.05) {
-  For ($delay=0; $delay -le 30; $delay++) {
-    For ($load=0.05; $load -le 0.65; $load+=0.05) {
-        python .\main.py $load 4 0 T $loss $delay
-    }
+For ($delay=0; $delay -le 50; $delay+=5) {
+  For ($load=0.05; $load -le 0.65; $load+=0.05) {
+      python .\main.py $load 4 0 T $delay
   }
 }
