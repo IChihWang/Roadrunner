2864d66138409517a8d77050abef976add00e6dd
Michael I.-C. Wang <icw238@nyu.edu>
2024-10-06 01:13:35 -0700

    Change the communication settings

--- communication.py
+++ communication.py
@@ -7,13 +7,10 @@ random.seed(0)
 # Input: two items (cars, facilities "AIM")
 # Output: communication delays in ms (INF for drop)
 def get_communication_delay(src_item, dst_item):
-    '''
     if random.choice([False]*19 + [True]):
         return float('inf')
     else:
         return random.randrange(0, 100, 1)*0.001 # ms
-    '''
-    return 0
 
 def get_retry_timeout(src_item, dst_item):
     return 0.01     # 10 ms
